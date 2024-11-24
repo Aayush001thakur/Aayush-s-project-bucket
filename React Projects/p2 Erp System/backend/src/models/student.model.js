1e752471e41e533a78290ef10bd9b56321d9017d
Aayush Kumar <thakuraayush5015@gmail.com>
2024-11-24 14:49:36 +0530

    backend filed setup

--- React Projects/p2 Erp System/backend/src/models/student.model.js
+++ React Projects/p2 Erp System/backend/src/models/student.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
 const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   rollNumber: { type: String, unique: true, required: true },
@@ -7,6 +8,7 @@ const studentSchema = new mongoose.Schema({
   age: { type: Number, required: true },
   gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
   address: { type: String },
+  password: {type: String},
   contactNumber: { type: String, required: true },
   guardian: {
     name: { type: String },
@@ -15,5 +17,48 @@ const studentSchema = new mongoose.Schema({
   },
   createdAt: { type: Date, default: Date.now },
 });
+
+studentSchema.pre("save", async function (next){
+  if(!this.isModified("password") ) return next();
+  this.password = bcrypt(this.password.hash, 10)
+  next()
+})
+
+studentSchema.methods.isPasswordCorrect = async function (password) {
+   return await bcrypt.compare(password, this.password)
+
+}
+
+studentSchema.methods.generateAcessToken =  function () {
+   return  jsonwebtoken.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        name: this.name 
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      }
+    )
+
+
+}
+studentSchema.methods.generateRefreshToken = async function () {
+  return  jsonwebtoken.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+
+
+
+
+}
+
 
 export const Student = mongoose.model("Student", studentSchema);
--- React Projects/p2 Erp System/backend/src/models/teacher.model.js
+++ React Projects/p2 Erp System/backend/src/models/teacher.model.js
@@ -7,9 +7,50 @@ const teacherSchema = new mongoose.Schema({
     classAssigned: [{ type: mongoose.Schema.Types.ObjectId, ref: "Class" }],
     contactNumber: { type: String, required: true },
     email: { type: String, unique: true },
+    password:{type: String},
     address: { type: String },
     joinDate: { type: Date, default: Date.now },
   });
   
   export const Teacher = mongoose.model("Teacher", teacherSchema);
   
+teacherSchema.pre("save", async function (next){
+   if(!this.isModified(this.password)) return next();
+   this.password = bcrypt(this.password.hash, 10)
+   next()
+})
+
+teacherSchema.methods.isPasswordCorrect = async function (password) {
+  await bcrypt.compare(password, this.password)
+}
+
+teacherSchema.methods.generateAcessToken =  function () {
+  return  jsonwebtoken.sign(
+     {
+       _id: this._id,
+       email: this.email,
+       name: this.name 
+     },
+     process.env.ACCESS_TOKEN_SECRET,
+     {
+       expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+     }
+   )
+
+
+}
+teacherSchema.methods.generateRefreshToken = async function () {
+ return  jsonwebtoken.sign(
+   {
+     _id: this._id,
+   },
+   process.env.REFRESH_TOKEN_SECRET,
+   {
+     expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+   }
+ )
+
+
+
+
+}
